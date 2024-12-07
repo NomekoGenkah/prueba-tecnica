cbd734d502f78d0bd6cb971bbb28453374f15b2b
NomekoGenkah <166954170+NomekoGenkah@users.noreply.github.com>
2024-12-07 13:07:06 -0300

    model -> user.js

---
+++ model/user.js
@@ -0,0 +1,54 @@
+
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize) => {
+    class User extends Model{
+
+    }
+
+    User.beforeCreate(async (user) =>{
+        if(user.password){
+            const passwordHash = await bcrypt.hash(user.password, 10);
+            user.password = passwordHash;
+        }
+    });
+
+    User.init({
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        nombre:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: true,
+        },
+
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        rol:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: true,
+        },
+    },
+    {sequelize,
+        modelName: 'User',
+        tableName: 'usuarios',
+        timestamps: true,
+    });
+
+    return User;
+}
